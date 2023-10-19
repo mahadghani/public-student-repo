a1202fbd622f583295ac033239a0bd734a99f5d8
mahadghani <mghani@pdx.edu>
2023-10-19 02:22:51 -0700

    Homework 2

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,29 @@
 // Add your code here
+const input = document.querySelector("input");
+input.addEventListener("input", handleKeyDown);
 
-input.addEventListener('keydown', handleKeyDown);
+const toHighlight = document.querySelector(".myText");
+const originalContent = toHighlight.innerHTML;
+
+function handleKeyDown(event) {
+  // Code to handle input
+  toHighlight.innerHTML = originalContent;
+
+  const coloredWord = input.value;
+  const currContent = toHighlight.innerHTML;
+
+  // Use a regular expression that puts to strings to be highlighted in span tags
+  const highlightedContent = currContent.replace(
+    new RegExp(`(${coloredWord})`, "gi"),
+    '<span class="highlight">$1</span>'
+  );
+
+  // Update the content of the div with the highlighted version
+  toHighlight.innerHTML = highlightedContent;
+
+  // Go through and actually highlight the words now
+  const wordHighlight = document.querySelectorAll(".highlight");
+  wordHighlight.forEach((element) => {
+    element.style.backgroundColor = "yellow";
+  });
+}
